4c2dd03772ecfc71a83c5378ee386c09cb27f627
Ricardo Barbosa <157282194+ricardobarbosa-dev@users.noreply.github.com>
2025-07-11 21:14:58 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,6 @@ const markdownToHTML = (text) => {
   return converter.makeHtml(text);
 };
 
-// AIzaSyAWUMezIeBlBTxNje_8v_cDPpxf_2zjV5o
 const perguntarIA = async (question, game, apiKey) => {
   const model = "gemini-2.0-flash";
   const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
